c1e2c62b22f4663efb59a77ef1f74116e9c00706
yuunseo <dbstj7457@naver.com>
2025-07-24 15:40:29 +0900

    main page, lonin, signup page

---
+++ on-fe/app/(main)/page.js
@@ -0,0 +1,45 @@
+'use client'
+
+import {React,useState} from 'react';
+import FlyingStar from '../ui/background/FlyingStar.jsx';
+import './main.css';
+import Link from 'next/link.js';
+import Image from 'next/image.js';
+import Header from '../components/Header.jsx';
+
+const Main = () => {
+
+  const title = "ON";
+  const sub_title = "Learn Together, Live Together";
+  const slogan = "단순한 채팅이 아니라, 같은 공간에 함께 있다는 존재감으로 학습 동기부여를 받아보세요!";
+
+  const [ison,setOn] = useState(false);
+
+  return (
+    <div className="container">
+
+      {/* 헤더 */}
+      <Header/>
+
+      {/* 별 배경 */}
+      <FlyingStar />
+
+      {/* 본문 콘텐츠 */}
+      <div className="content">
+          <Image 
+          src="/assets/on_icon.svg"
+          width={500}
+          height={500}
+          alt='on'/>
+        <div className="text-section">
+          <div className="main-title">{sub_title}</div>
+          <div className="subtitle">{title}</div>
+          <div className="description">{slogan}</div>
+           <Link href="/login" className='button'>시작하기</Link>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Main;
---
+++ on-fe/app/components/Header.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import Link from 'next/link';
+import Image from 'next/image';
+import './header.css'
+
+const Header = () => {
+  return (
+    
+    <div className="headers">
+        <Link href="/" className="logo">ON</Link>
+        <Link href="/login" className="login">login</Link>
+        
+    </div>
+
+  )
+}
+
+export default Header
