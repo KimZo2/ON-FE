bb6b867412d84951c7340fc5bf4cbbc335a86d96
Bellmin <whdals011203@naver.com>
2025-07-24 16:14:34 +0900

    refactor: bg 컨테이너 레이아웃 수정

--- on-fe/app/(auth)/login/page.js
+++ on-fe/app/(auth)/login/page.js
@@ -4,26 +4,25 @@ import './form.css';
 export default function LogInPage() {
     return (
         <div className='bg'>
-        <div className="signup-form">
-            <div className='login_buttons'>
+            <div className="signup-form">
+                <div className='login_buttons'>
 
-                <p className="logo">ON</p>
-                <div className="kakao">
-                    <div className='kakao_icon'></div>
-                    카카오 로그인</div>
-                <div className="google">
-                    <div className='google_icon'></div>
-                    구글 로그인</div>
-                <div className="naver">
-                    <div className='naver_icon'></div>
-                    네이버 로그인</div>
-                <div className='github'>
-                    <div className='github_icon'></div>
-                    깃허브 로그인</div>
+                    <p className="logo">ON</p>
+                    <div className="kakao">
+                        <div className='kakao_icon'></div>
+                        카카오 로그인</div>
+                    <div className="google">
+                        <div className='google_icon'></div>
+                        구글 로그인</div>
+                    <div className="naver">
+                        <div className='naver_icon'></div>
+                        네이버 로그인</div>
+                    <div className='github'>
+                        <div className='github_icon'></div>
+                        깃허브 로그인</div>
 
+                </div>
             </div>
-            
-        </div>
         </div>
     );
 }
