2d49feaf213576a930ed94b47b46f04881feed90
yuunseo <dbstj7457@naver.com>
2025-07-27 20:54:14 +0900

    refactor: LoginForm(로그인 페이지) 스타일링 변경: css -> tailwind

--- on-fe/app/(auth)/login/page.js
+++ on-fe/app/(auth)/login/page.js
@@ -1,7 +1,6 @@
 'use client';
 import LoginForm from '@/components/auth/LoginForm';
 import FormLayOut from '@/components/form/FormLayOut';
-import './form.css';
 
 export default function LogInPage() {
 
