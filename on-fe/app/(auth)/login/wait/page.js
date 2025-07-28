d714f070c4a0ddde65632a6b64974de0bfc09d8f
Bellmin <whdals011203@naver.com>
2025-07-28 10:51:42 +0900

    refactor: API 호출 로직 분리

--- on-fe/app/(auth)/login/wait/page.js
+++ on-fe/app/(auth)/login/wait/page.js
@@ -4,6 +4,7 @@ import { redirect, useRouter, useSearchParams } from 'next/navigation'
 import LoadingSpinner from '@/components/loading/LoadingSpinner'
 import axios from 'axios'
 import { saveAccessToken, saveNickName } from '@/util/AuthUtil'
+import { getKakaoAccessToken, sendAccessToken } from '@/apis/auth'
 
 const SERVER_URL = process.env.NEXT_PUBLIC_BE_SERVER_URL;
 const CLIENT_ID = process.env.NEXT_PUBLIC_KAKAO_CLIENT_ID;
@@ -23,19 +24,13 @@ export default function OAuthCallbackPage() {
 
     ; (async () => {
       try {
-        // sendTempCode를 여기서 정의하거나, 필요한 값을 인자로 넘깁니다.
-        const accessToken = await fetchKakaoAccessToken(code)
-          .then(response => response.data)
-          .then(data => data.access_token)
+        const accessToken = await getKakaoAccessToken(code)
           .catch(error => {
             console.log(error);
             router.replace("/login");
           });
 
-        console.log(accessToken);
-
-        const response = await sendLoginRequest({ oauthType, accessToken });
-        const { token, nickname } = response.data;
+        const { token, nickname } = await sendAccessToken({ oauthType, accessToken });
 
         saveAccessToken(token);
         saveNickName(nickname);
@@ -44,7 +39,7 @@ export default function OAuthCallbackPage() {
 
       } catch (err) {
         if (err.status === 428) {
-          const {provider, providerId} = err.response.data;
+          const { provider, providerId } = err.response.data;
           const query = new URLSearchParams({ provider, providerId }).toString()
           router.replace(`/login/additional-info?${query}`)
         }
@@ -60,30 +55,5 @@ export default function OAuthCallbackPage() {
     <div className="flex items-center justify-center h-screen">
       <LoadingSpinner />
     </div>
-  )
-}
-
-const fetchKakaoAccessToken = (code) => {
-  const data = {
-    code,
-    grant_type: "authorization_code",
-    client_id: CLIENT_ID,
-    redirect_uri: KAKAO_REDIRECT_URL
-  }
-
-  return axios.post(
-    "https://kauth.kakao.com/oauth/token",
-    data,
-    {
-      headers:
-        { 'content-type': 'application/x-www-form-urlencoded;charset=UTF-8' }
-    }
-  )
-}
-
-const sendLoginRequest = ({ oauthType, accessToken }) => {
-  return axios.post(
-    `${SERVER_URL}/auth/login/${oauthType}`,
-    { accessToken: accessToken }
   )
 }
