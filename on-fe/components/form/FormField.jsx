dab71a275e38647ba1ceee863a3b6360797091ef
rlajm1203 <whdals011203@naver.com>
2025-08-13 17:16:48 +0900

    refactor: FormField props 추가

--- on-fe/components/form/FormField.jsx
+++ on-fe/components/form/FormField.jsx
@@ -1,9 +1,9 @@
 // components/FormField.jsx
 import React from 'react'
 
-export default function FormField({ label, type = 'text', name, value, onChange, maxLength, required }) {
+export default function FormField({ label, type = 'text', name, value, onChange, maxLength, required, className }) {
   return (
-    <div>
+    <div className={className}>
       <label className="title">{label}</label>
       <input
         className="input mt-2 w-full px-5 py-3 rounded-xl text-black bg-white placeholder:text-gray-400 focus:outline-none"
