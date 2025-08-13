4bd344c1e345860c9eed9f7e63616bb776ed7c9a
rlajm1203 <whdals011203@naver.com>
2025-08-13 22:06:28 +0900

    refactor: 불필요한 import 제거

--- on-fe/app/room/join/page.jsx
+++ on-fe/app/room/join/page.jsx
@@ -1,8 +1,6 @@
 'use client'
 
 import React from 'react'
-import Header from '@/components/Header'
-import FlyingStar from '@/components/background/FlyingStar'
 import DefaultPageFrame from '@/components/DefaultPageFrame'
 
 const page = () => {
