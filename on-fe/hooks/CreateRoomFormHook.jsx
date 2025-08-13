f304c6793ffd26841b07b8ba9ca21711bf1f17a3
rlajm1203 <whdals011203@naver.com>
2025-08-13 17:14:34 +0900

    feat: CreateRoomFormHook 추가

---
+++ on-fe/hooks/CreateRoomFormHook.jsx
@@ -0,0 +1,55 @@
+// hooks/useAdditionalInfoForm.js
+import { useState } from 'react'
+import axios from 'axios'
+import { useRouter, useSearchParams } from 'next/navigation'
+
+export function useCreateRoom() {
+  const router = useRouter()
+
+  const [form, setForm] = useState({
+    roomName: '',
+    capacity: '',
+    duration: '',
+    visibility: false,
+    password : ''
+  })
+  
+  const [isSubmitting, setIsSubmitting] = useState(false)
+
+  const handleChange = (e) => {
+    const { name, type, value, checked } = e.target
+    setForm(prev => ({
+      ...prev,
+      [name]: type === 'checkbox' ? checked : value,
+    }))
+  }
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    if (!form.roomName || !form.capacity || !form.duration || !form.password) {
+      alert('필수 항목을 올바르게 입력해 주세요.')
+      return
+    }
+
+    setIsSubmitting(true)
+    try {
+      const payload = { provider, providerId, ...form }
+      const res = await axios.post( // TODO: axios 객체 커스텀 하기
+        `${process.env.NEXT_PUBLIC_BE_SERVER_URL}/room`, // TODO: API URI 수정하기
+        payload
+      )
+      if (res.status===201) {
+        alert('방 생성 성공!')
+        router.push('/') // TODO: 생성한 방으로 이동하거나, 메인페이지로 이동하거나 
+      } else {
+        throw new Error('서버 응답 오류')
+      }
+    } catch {
+      alert('방 생성에 실패하였습니다.')
+    } finally {
+      setIsSubmitting(false)
+    }
+  }
+
+  return { form, isSubmitting, handleChange, handleSubmit }
+}
