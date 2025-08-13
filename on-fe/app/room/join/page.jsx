bebc1d116e9ebeb5c509bae6ddc85e368365780c
yuunseo <dbstj7457@naver.com>
2025-08-13 23:04:28 +0900

    feat: /room/join page 구성하기

--- on-fe/app/room/join/page.jsx
+++ on-fe/app/room/join/page.jsx
@@ -2,11 +2,17 @@
 
 import React from 'react'
 import DefaultPageFrame from '@/components/DefaultPageFrame'
+import FormLayOut from '@/components/form/FormLayOut'
+import JoinRoomForm from '@/components/form/JoinRoomForm'
 
 const page = () => {
   return (
     <DefaultPageFrame>
+      <FormLayOut className=''>
+        <JoinRoomForm className='w-[15dvw] flex flex-col justify-between gap-3'>
+        </JoinRoomForm>
 
+      </FormLayOut>
     </DefaultPageFrame>
   )
 }
