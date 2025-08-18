98ce4fae0a0a846af3977582f9e84276a852f3a3
rlajm1203 <whdals011203@naver.com>
2025-08-18 17:20:40 +0900

    feat: useUserInfo 훅 추가

---
+++ on-fe/hooks/user/useUserInfo.jsx
@@ -0,0 +1,19 @@
+import { getNickName, isLoggedIn } from '@/util/AuthUtil';
+import { useLayoutEffect } from 'react';
+import { useState } from 'react'
+
+const useUserInfo = () => {
+
+    const [nickName, setNickName] = useState(null);
+
+    useLayoutEffect(() => {
+        if (isLoggedIn()) {
+            const n = getNickName();
+            setNickName(n);
+        };
+    })
+
+    return [nickName];
+}
+
+export default useUserInfo
