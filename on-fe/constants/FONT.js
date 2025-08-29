98b202d8fa2a9fe0dd33dba345c55dbe645bccd8
yuunseo <dbstj7457@naver.com>
2025-08-29 17:25:07 +0900

    font: pretendard 폰트 추가

--- on-fe/constants/FONT.js
+++ on-fe/constants/FONT.js
@@ -1,5 +1,7 @@
 // app/some/page.tsx (또는 해당 컴포넌트 파일)
 import { Prompt, Press_Start_2P } from 'next/font/google';
+import localFont from 'next/font/local'; // localFont 임포트
+
 
 export const prompt = Prompt({
   subsets: ['latin'],
@@ -12,4 +14,11 @@ export const pressStart2P = Press_Start_2P({
   weight: '400',       
   subsets: ['latin'],
   display: 'swap',
+});
+
+// Pretendard 폰트 추가
+export const pretendard = localFont({
+  src: '../../public/fonts/Pretendard-Medium.otf',
+  display: 'swap',
+  variable: '--font-pretendard', // CSS 변수명 지정 (선택 사항)
 });
