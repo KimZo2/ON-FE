936d726cd30592bdfc317571ad552c30b9cc79ab
yuunseo <dbstj7457@naver.com>
2025-08-29 17:30:08 +0900

    chore: 불필요한 import 삭제

--- on-fe/app/layout.js
+++ on-fe/app/layout.js
@@ -1,6 +1,5 @@
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { pressStart2P, pretendard } from "@/constants/FONT";
+import { pretendard } from "@/constants/FONT";
 
 export default function RootLayout({ children }) {
   return (
