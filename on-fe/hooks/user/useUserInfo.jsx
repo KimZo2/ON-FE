2347c150f33de3f46149c27747686ab56cc8ecf7
rlajm1203 <whdals011203@naver.com>
2025-10-01 12:30:45 +0900

    refactor: 내 정보  가져오는 훅으로 수정

--- on-fe/hooks/user/useUserInfo.jsx
+++ on-fe/hooks/user/useUserInfo.jsx
@@ -1,19 +1,24 @@
-import { getNickName, isLoggedIn } from '@/util/AuthUtil';
+import { backendApiInstance } from '@/apis/instance';
+import ROUTES from '@/constants/ROUTES';
+import { isLoggedIn } from '@/util/AuthUtil';
 import { useLayoutEffect } from 'react';
 import { useState } from 'react'
 
 const useUserInfo = () => {
 
+    const [userId, setUserId] = useState(null);
     const [nickName, setNickName] = useState(null);
 
     useLayoutEffect(() => {
-        if (isLoggedIn()) {
-            const n = getNickName();
+        if (isLoggedIn()) { // 로그인 되어 있으면
+            const { userId, nickname } = backendApiInstance.get(ROUTES.MYINFO);
+            const n = nickname;
+            setUserId(userId);
             setNickName(n);
         }
-    }, [])
+    }, []);
 
-    return [nickName];
+    return {userId, nickName};
 }
 
 export default useUserInfo
