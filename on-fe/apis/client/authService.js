eacd7bc76cbaf71f7968fb47a36a36c93be0602a
yuunseo <dbstj7457@naver.com>
2025-12-18 14:07:17 +0900

    feat: authService에서 로그인, 로그아웃 API 호출 담당하도록 분리

---
+++ on-fe/apis/client/authService.js
@@ -0,0 +1,27 @@
+import API from "@/constants/API";
+import { publicApiInstance } from "../instances/publicApiInstance";
+import { clientApiInstance } from "../instances/clientApiInstance";
+
+/**
+ * OAuth 로그인
+ */
+export const login = ({ oauthType, code }) => {
+  return publicApiInstance.get(API.AUTH.OAUTH_LOGIN(oauthType), {
+    params: { code },
+  });
+};
+
+/**
+ * 로그아웃 요청 (Refresh Token 삭제)
+ */
+export const logoutRequest = async () => {
+  try {
+    // interceptor에서 response.data를 반환하므로
+    // 여기서 받는 값은 이미 "data"
+    const data = await clientApiInstance.post(API.AUTH.LOGOUT);
+    return data;
+  } catch (error) {
+    console.error("로그아웃 요청 실패:", error);
+    throw error;
+  }
+};
