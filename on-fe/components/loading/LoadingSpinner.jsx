dba35b3fcac2840e6ec3e34635cf4db80b90e18c
yuunseo <dbstj7457@naver.com>
2025-12-18 16:08:04 +0900

    style: 로딩 스피너 배경화면 흰 색에서 검은 색으로 변경

--- on-fe/components/loading/LoadingSpinner.jsx
+++ on-fe/components/loading/LoadingSpinner.jsx
@@ -3,7 +3,7 @@
 import React from 'react'
 import './spinner.css'
 
-export default function LoadingSpinner({message="서버의 응답을 기다리는 중입니다..", color="black"}) {
+export default function LoadingSpinner({message="서버의 응답을 기다리는 중입니다..", color="white"}) {
   return (
     <div className="spinner-wrapper">
       <div className="spinner"></div>
