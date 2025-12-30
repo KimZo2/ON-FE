22e56359c2581d051371e46188842d0e93e154fd
yuunseo <dbstj7457@naver.com>
2025-12-30 15:18:14 +0900

    refactor: 회원가입 리다이렉트 페이지 변경으로 인한 수정 - additional info page에서 sign up page

--- on-fe/app/(auth)/signup/page.jsx
+++ on-fe/app/(auth)/signup/page.jsx
@@ -1,5 +1,18 @@
+import { Suspense } from 'react';
+import AdditionalInfoForm from '@/components/form/AdditionalInfoForm';
+import FormLayout from '@/components/form/FormLayOut';
+import FlyingStar from '@/components/background/FlyingStar';
+import LoadingSpinner from '@/components/loading/LoadingSpinner';
+
 export default function SignUpPage() {
     return (
-        <div>회원가입</div>
+        <div className="min-h-screen relative overflow-hidden bg-black">
+            <FlyingStar />
+            <FormLayout>
+                <Suspense fallback={<LoadingSpinner />}>
+                    <AdditionalInfoForm/>
+                </Suspense>
+            </FormLayout>
+        </div>
     );
 }
