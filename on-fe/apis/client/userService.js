159dda10f7943ea87b15cd843e25932cb502c6ca
yuunseo <dbstj7457@naver.com>
2025-12-30 15:21:10 +0900

    chore: 상수 경로 변경(USER.ADDITIONAL_INFO에서 AUTH.SIGNUP)

--- on-fe/apis/client/userService.js
+++ on-fe/apis/client/userService.js
@@ -1,7 +1,6 @@
 import { clientApiInstance } from '@/apis/instances/clientApiInstance'
 import { publicApiInstance } from '@/apis/instances/publicApiInstance'
 import API from '@/constants/API'
-import ROUTES from '@/constants/ROUTES'
 
 export const userService = {
   // 내 정보 조회
@@ -11,6 +10,6 @@ export const userService = {
 
   // 추가 정보 입력(소셜 회원가입 완료)
   signup(payload) {
-    return publicApiInstance.post(API.USER.ADDITIONAL_INFO, payload)
+    return publicApiInstance.post(API.AUTH.SIGNUP, payload)
   },
 }
