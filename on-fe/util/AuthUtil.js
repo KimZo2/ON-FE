a1c186d1a245365fd168ba495c5e95fd7928c03f
yuunseo <dbstj7457@naver.com>
2025-12-30 15:14:33 +0900

    refactor: 소셜 로그인 로직 변경으로 인한 수정 - 서버로 페이지 이동

--- on-fe/util/AuthUtil.js
+++ on-fe/util/AuthUtil.js
@@ -1,12 +1,3 @@
-const KAKAO_CLIENT_ID = process.env.NEXT_PUBLIC_KAKAO_CLIENT_ID;
-const KAKAO_REDIRECT_URI = process.env.NEXT_PUBLIC_KAKAO_REDIRECT_URI;
-const NAVER_CLIENT_ID = process.env.NEXT_PUBLIC_NAVER_CLIENT_ID;
-const NAVER_REDIRECT_URI = process.env.NEXT_PUBLIC_NAVER_REDIRECT_URI;
-const GOOGLE_CLIENT_ID = process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID;
-const GOOGLE_REDIRECT_URI = process.env.NEXT_PUBLIC_GOOGLE_REDIRECT_URI;
-const GITHUB_CLIENT_ID = process.env.NEXT_PUBLIC_GITHUB_CLIENT_ID;
-const GITHUB_REDIRECT_URI = process.env.NEXT_PUBLIC_GITHUB_REDIRECT_URI;
-
 // URL-safe 토큰 (base64url). 기본 32바이트 ≈ 43자
 export function cryptoRandom(bytes = 32) {
   const buf = new Uint8Array(bytes);
@@ -18,49 +9,11 @@ export function cryptoRandom(bytes = 32) {
 
 
 export function handleKakao() {
-  window.location.href = `https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=${KAKAO_CLIENT_ID}&redirect_uri=${KAKAO_REDIRECT_URI}`
-}
-
-export function handleNaver() {
-  const state = cryptoRandom();
-  sessionStorage.setItem('naver_state', state);
-
-  const params = new URLSearchParams({
-    client_id: NAVER_CLIENT_ID,
-    redirect_uri: NAVER_REDIRECT_URI,
-    state: state
-  });
-  window.location.href = `https://nid.naver.com/oauth2.0/authorize?response_type=code&${params.toString()}`
-}
-
-export function handleGoogle() {
-  const state = cryptoRandom();
-  sessionStorage.setItem('google_state', state);
-
-  const params = new URLSearchParams({
-    client_id: GOOGLE_CLIENT_ID,
-    redirect_uri: GOOGLE_REDIRECT_URI, 
-    response_type: 'code',
-    scope: 'openid email profile',     
-    access_type: 'offline',
-    prompt: 'consent',
-    state
-  });
-  window.location.href = `https://accounts.google.com/o/oauth2/v2/auth?${params.toString()}`;
+  window.location.href = `http://localhost:8080/oauth2/authorization/kakao`
 }
 
 export function handleGithub() {
-
-  const state = cryptoRandom();
-  sessionStorage.setItem('gh_state', state);
-
-  const params = new URLSearchParams({
-    client_id: GITHUB_CLIENT_ID,
-    redirect_uri: GITHUB_REDIRECT_URI,
-    state,
-    scope: 'read:user user:email',
-  });
-  window.location.href = `https://github.com/login/oauth/authorize?${params.toString()}`;
+  window.location.href = `http://localhost:8080/oauth2/authorization/github`;
 }
 
 
