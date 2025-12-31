280d52e307128088075fbf88be5b55593787bf47
yuunseo <dbstj7457@naver.com>
2025-12-31 14:11:13 +0900

    feat: 방생성하기 입력 폼 에러메세지 추가 & alert 대신 toast로 변경

--- on-fe/hooks/room/useCreateRoom.jsx
+++ on-fe/hooks/room/useCreateRoom.jsx
@@ -3,6 +3,7 @@ import { useRouter } from 'next/navigation'
 import { roomService } from '@/apis/client/roomService'
 import ROUTES from '@/constants/ROUTES'
 import { getNickname } from '@/util/AuthUtil'
+import { toast } from 'react-hot-toast'
 
 export function useCreateRoom({ onFormSubmissionStart, onFormSubmissionComplete } = {}) {
   const router = useRouter()
@@ -15,6 +16,11 @@ export function useCreateRoom({ onFormSubmissionStart, onFormSubmissionComplete 
   })
   
   const [isSubmitting, setIsSubmitting] = useState(false)
+  const [errors, setErrors] = useState({
+    maxParticipants: '',
+    roomTime: '',
+  })
+
 
   const handleChange = (e) => {
     const { name, value } = e.target
@@ -26,33 +32,44 @@ export function useCreateRoom({ onFormSubmissionStart, onFormSubmissionComplete 
 
   const handleSubmit = async (e) => {
     e.preventDefault()
-    if (!form.name || !form.maxParticipants || !form.roomTime) {
-      alert('필수 항목을 올바르게 입력해 주세요.')
-      return
+    
+    const newErrors = {}
+    const max = Number(form.maxParticipants)
+    const time = Number(form.roomTime)
+
+    // TODO: 인원수 결정 필요
+    if (max < 2 || max > 10) {
+      newErrors.maxParticipants = '인원 수는 2~10명 사이여야 합니다.' 
     }
 
+    // TODO: 시간 결정 필요
+    if (time < 1 || time > 120) {
+      newErrors.roomTime = '시간은 1시간~12시간 사이여야 합니다.'
+    }
+
+    if (Object.keys(newErrors).length > 0) {
+      setErrors(newErrors)
+      return
+    }
+    setErrors({})
     onFormSubmissionStart?.()
     setIsSubmitting(true)
+
     try {
       const payload = { ...form, isPrivate: false, password: '' }
       const res = await roomService.create(payload)
-      // TODO: API 응답 구조에 맞게 수정 필요
-      alert('방 생성 성공!')
+
+      // 성공 Toast
+      toast.success('방이 생성되었습니다!')
       router.push(ROUTES.ROOM)
       onFormSubmissionComplete?.()
-      // if (res.status===201) {
-      //   alert('방 생성 성공!')
-      //   router.push(ROUTES.ROOM)
-      //   onFormSubmissionComplete?.()
-      // } else {
-      //   throw new Error('서버 응답 오류')
-      // }
     } catch {
-      alert('방 생성에 실패하였습니다.')
+      // 실패 Toast
+      toast.error('방 생성에 실패했습니다.')
     } finally {
       setIsSubmitting(false)
     }
   }
 
-  return { form, isSubmitting, handleChange, handleSubmit }
+  return { form, errors, isSubmitting, handleChange, handleSubmit }
 }
