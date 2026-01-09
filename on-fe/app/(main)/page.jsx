cf72f6b15997d237ed20ba6a4507c06eeb4609e6
yuunseo <dbstj7457@naver.com>
2026-01-09 16:56:16 +0900

    refactor: 조건부 렌더링 제거로 hydration 불일치 문제 해결

--- on-fe/app/(main)/page.jsx
+++ on-fe/app/(main)/page.jsx
@@ -1,18 +1,12 @@
 'use client'
-
-import { React } from 'react';
 import Link from 'next/link.js';
 import Image from 'next/image.js';
-import { isLoggedIn } from '@/util/AuthUtil.js';
 import DefaultPageFrame from '@/components/DefaultPageFrame.jsx';
 import ROUTES from '@/constants/ROUTES.js';
 import { pressStart2P } from '@/constants/FONT';
 
 
 const Main = () => {
-
-  const isLogin = isLoggedIn();    
-
   const title = "ON";
   const sub_title = "Learn Together, Live Together";
   const slogan = "단순한 채팅이 아니라, 같은 공간에 함께 있다는 존재감으로 학습 동기부여를 받아보세요!";
@@ -32,7 +26,7 @@ const Main = () => {
           <div className={`${pressStart2P.className} text-[3rem] leading-[1.1] bg-gradient-to-r from-white to-gray-300 bg-clip-text text-transparent`}>{title}</div>
           <div className="text-gray-300 leading-relaxed px-[5rem]">{slogan}</div>
           <Link
-            href={isLogin ? ROUTES.ROOM : ROUTES.LOGIN}
+            href={ROUTES.ROOM}
             className="px-[2rem] py-[1rem] text-white rounded-xl bg-gray-500/70 transition-all duration-300 ease-in-out cursor-pointer overflow-hidden
                 hover:-translate-y-0.5 hover:shadow-[0_20px_25px_rgba(0,0,0,0.15)]"
           >시작하기</Link>
